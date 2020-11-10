9f54df5adce97fe643d3b733374be297fe2cd839
Luke I. Wilson <thelukaswils@gmail.com>
2020-11-10 16:57:42 -0600

    Made menu functional

--- assets/scripts/Menu.gd
+++ assets/scripts/Menu.gd
@@ -7,3 +7,28 @@ func _ready() -> void:
 
 func _on_Copyright_meta_clicked(meta) -> void:
 	OS.shell_open(meta) # Open the link in user's browser
+
+
+func _on_SingleplayerButton_pressed() -> void:
+	# TODO: show a drop down for difficulty options
+	get_tree().change_scene("res://assets/scenes/Game.tscn")
+
+
+func _on_SoundsButton_toggled(button_pressed: bool) -> void:
+	Globals.sounds = button_pressed
+
+
+func _on_MusicButton_toggled(button_pressed: bool) -> void:
+	Globals.music = button_pressed
+
+
+func _on_FullscreenButton_toggled(button_pressed: bool) -> void:
+	OS.window_fullscreen = button_pressed
+
+
+func _on_BorderlessButton_toggled(button_pressed: bool) -> void:
+	OS.window_borderless = button_pressed
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
---
+++ assets/scripts/globals.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var sounds: bool = true # Whether the game should play sounds
+var music: bool = true # Whether the game should play music
