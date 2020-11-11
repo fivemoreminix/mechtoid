c8daabc37a0209cf8fa25a6a56625d86a77045c1
shanks-red <lezharayman@gmail.com>
2020-11-11 03:01:17 +0100

    added the win lose screen !

---
+++ assets/scripts/Game.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_LeftPlayer_killed():
+	get_tree().call_group("Checkwinner", "get_winner", "alien")
+
+
+func _on_LeftPlayer2_killed():
+	get_tree().call_group("Checkwinner", "get_winner", "human")
