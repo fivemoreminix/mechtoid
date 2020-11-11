5fa4b5723b4c2a9cc0c5e61acad05bbcdf219f38
shanks-red <lezharayman@gmail.com>
2020-11-11 18:49:41 +0100

    some features added to the healthBar and energyBar

--- assets/scenes/EnergyBar.gd
+++ assets/scenes/EnergyBar.gd
@@ -4,6 +4,36 @@ onready var energy_bar_under = $HealthBarUnder
 onready var energy_bar = $HealthBar
 onready var update_tween = $Tween
 var default_max_value
+var no_energy = false
+
+export (Color) var danger_color_over = Color()
+export (Color) var normal_color_over = Color()
+
+export (Color) var danger_color_progress = Color()
+export (Color) var normal_color_progress = Color()
+
+export (float,0 , 1, 0.05) var danger_zone = 0.2
+
+
+func assign_energy_color(energy):
+	#wait till enegry is full again 
+	if no_energy:
+		energy_bar.tint_progress = danger_color_progress
+		energy_bar.tint_over = danger_color_over
+		if energy == default_max_value:
+			no_energy = false
+		else:
+			return
+		
+	if energy < energy_bar.max_value * danger_zone:
+		energy_bar.tint_progress = danger_color_progress
+		energy_bar.tint_over = danger_color_over
+	else:
+		energy_bar.tint_progress = normal_color_progress
+		energy_bar.tint_over = normal_color_over
+	if energy == 0:
+		no_energy = true
+
 
 func _ready():
 	update_max_energybar(default_max_value)
@@ -13,6 +43,7 @@ func update_energy_bars(energy):
 	energy_bar.value = energy
 	update_tween.interpolate_property(energy_bar_under, "value", energy_bar_under.value, energy, 0.0, Tween.TRANS_SINE)
 	update_tween.start()
+	assign_energy_color(energy)
 
 func update_max_energybar(max_energy):
 	energy_bar.max_value = max_energy
--- assets/scripts/HealthBar.gd
+++ assets/scripts/HealthBar.gd
@@ -5,6 +5,24 @@ onready var health_bar_under = $HealthBarUnder
 onready var update_tween = $Tween
 var default_max_value
 
+export (Color) var danger_color_over = Color()
+export (Color) var normal_color_over = Color()
+
+export (Color) var danger_color_progress = Color()
+export (Color) var normal_color_progress = Color()
+
+export (float,0 , 1, 0.05) var danger_zone = 0.2
+
+func assign_health_color(health):
+	if health < health_bar.max_value * danger_zone:
+		health_bar.tint_progress = danger_color_progress
+		health_bar.tint_over = danger_color_over
+	else:
+		health_bar.tint_progress = normal_color_progress
+		health_bar.tint_over = normal_color_over
+
+
+
 func _ready():
 	update_max_healthbar(default_max_value)
 
@@ -18,6 +36,7 @@ func update_health_bars(health):
 	health_bar.value = health
 	update_tween.interpolate_property(health_bar_under, "value", health_bar_under.value, health, 0.4, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.4)
 	update_tween.start()
+	assign_health_color(health)
 
 func _on_LeftPlayer_health_updated(health):
 	update_health_bars(health)
